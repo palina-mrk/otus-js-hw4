7af60dbebb70f4794af672f719e1c1e4c5344a87
palina-mrk <khudziakova@tut.by>
2025-08-04 11:19:31 +0300

    add sanity-check.yml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,5 @@
 import js from "@eslint/js";
 import globals from "globals";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
-import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -13,34 +10,4 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.jsonc"],
-    plugins: { json },
-    language: "json/jsonc",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.json5"],
-    plugins: { json },
-    language: "json/json5",
-    extends: ["json/recommended"],
-  },
-  {
-    files: ["**/*.md"],
-    plugins: { markdown },
-    language: "markdown/gfm",
-    extends: ["markdown/recommended"],
-  },
-  {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
-  },
 ]);
